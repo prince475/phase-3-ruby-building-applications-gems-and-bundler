25eee654883c4bed622e5e1f58e5e76f7a952a44
prince475 <danielmwai475@gmail.com>
2023-02-21 19:57:40 +0300

    Completed Assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,10 @@
 # Set up Bundler here following the instructions in the README
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+
+# the code above is equal to writting
+# require 'rspec'
+# requuire 'rest-client'
+# require 'pry'
